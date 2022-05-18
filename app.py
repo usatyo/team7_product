74deb96531dca089987618d94d12b5587f8ec492
usatyo <yu0424.math8128@gmail.com>
2022-05-18 15:12:29 +0900

    prototype

--- app.py
+++ app.py
@@ -49,12 +49,10 @@ def callback():
 
     return "OK"
 
-
 # @handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
 #     msg = event.message.text
 #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
-
 
 if __name__ == "__main__":
     app.run()
